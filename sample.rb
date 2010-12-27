ea31d6898091ee87ec240d695f9c0992fa86091e
tondol <tondol@gmail.com>
2010-12-28 06:17:31 +0900

    fix encoding of sample.rb

--- sample.rb
+++ sample.rb
@@ -26,14 +26,14 @@ puts song.to_s
 # >ruby -v
 # ruby 1.9.1p376 (2009-12-07 revision 26041) [i386-mswin32]
 # >ruby sample.rb
-# {"name"=>"�Ƃ�ǂ�", "level"=>"Lv 74 ���k�G�b�g", "point"=>"862VP", "module"=>"�i�`����
-# ��"}
-# {"name"=>"���͐푈", "thumbnail"=>"/divanet/img/pv/7dfafd6df0889e82", "easy"=>{"difficul
+# {"name"=>"とんどる", "level"=>"Lv 74 メヌエット", "point"=>"862VP", "module"=>"ナチュラ
+# ル"}
+# {"name"=>"恋は戦争", "thumbnail"=>"/divanet/img/pv/7dfafd6df0889e82", "easy"=>{"difficul
 # ty"=>"2", "clear"=>false, "great"=>false, "perfect"=>false, "trial"=>"none", "high_achiv
 # ement"=>"0.0%", "high_score"=>"0pts"}, "normal"=>{"difficulty"=>"4", "clear"=>false, "gr
 # eat"=>false, "perfect"=>false, "trial"=>"none", "high_achivement"=>"0.0%", "high_score"=
 # >"0pts"}, "hard"=>{"difficulty"=>"6", "clear"=>true, "great"=>true, "perfect"=>true, "tr
 # ial"=>"clear", "high_achivement"=>"103.88%", "high_score"=>"221990pts"}, "extreme"=>{"di
 # fficulty"=>"8", "clear"=>true, "great"=>true, "perfect"=>false, "trial"=>"clear", "high_
-# achivement"=>"87.66%", "high_score"=>"294700pts"}, "credit"=>{"��Ȏ�"=>"ryo", "�쎌��"=
-# >"ryo", "�ҋȎ�"=>"ryo", "�C���X�g���[�^�["=>"�O�֎m�Y"}}
+# achivement"=>"87.66%", "high_score"=>"294700pts"}, "credit"=>{"作曲者"=>"ryo", "作詞者"=
+# >"ryo", "編曲者"=>"ryo", "イラストレーター"=>"三輪士郎"}}
